9c43dc40711897ecc9ecf37c740be2c526488417
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-26 14:57:22 -0300

    Update StakingFactory.sol

--- contracts/StakingFactory.sol
+++ contracts/StakingFactory.sol
@@ -1,1 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "./StakingContract.sol";
+
+contract StakingFactory {
+    event StakingContractDeployed(address indexed contractAddress);
+
+    function deployStakingContract(address _drinkToken, address _dripsToken) external {
+        StakingContract stakingContract = new StakingContract(_drinkToken, _dripsToken);
+        emit StakingContractDeployed(address(stakingContract));
+    }
+}
