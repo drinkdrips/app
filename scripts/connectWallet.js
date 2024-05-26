a8f09afc98cfddb9d92f3104852d88ebbd3f7bf9
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-26 16:52:31 -0300

    Update connectWallet.js

--- scripts/connectWallet.js
+++ scripts/connectWallet.js
@@ -4,28 +4,15 @@ import { updateUIAfterConnect } from './uiUpdate.js';
 let web3;
 let userAccount;
 
-// Função para conectar a MetaMask
-async function connectMetaMask() {
-    try {
-        // Solicita ao usuário permissão para acessar sua conta MetaMask
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        // Atualiza a interface do usuário após a conexão bem-sucedida
-        await connectWallet();
-    } catch (error) {
-        console.error('Erro ao conectar à MetaMask:', error);
-    }
-}
-
-// Função para conectar a carteira
 export async function connectWallet() {
     if (window.ethereum) {
         try {
-            web3 = new Web3(window.ethereum);
+            web3 = new Web3(window.ethereum); // Web3 estará disponível globalmente
             await window.ethereum.request({ method: 'eth_requestAccounts' });
             const accounts = await web3.eth.getAccounts();
             userAccount = accounts[0];
-            await initContracts(web3, userAccount);
             updateUIAfterConnect(userAccount);
+            await initContracts(web3, userAccount);
             alert('Conexão realizada com sucesso!');
         } catch (error) {
             console.error(error);
@@ -36,7 +23,6 @@ export async function connectWallet() {
     }
 }
 
-// Adiciona um ouvinte de evento ao botão de conexão à MetaMask
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.connect-button').addEventListener('click', connectMetaMask);
+    document.querySelector('.connect-button').addEventListener('click', connectWallet);
 });
