c92eb9b8e29cd1b0c52c6579791de058d3f1cd33
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-26 15:10:36 -0300

    Update contractInteractions.js

--- scripts/contractInteractions.js
+++ scripts/contractInteractions.js
@@ -10,9 +10,9 @@ let stakingContract;
 
 async function initContracts() {
     web3 = new Web3(window.ethereum);
-    const DRINK_TOKEN_ADDRESS = "0x123..."; // Endereço do contrato DrinkToken
-    const DRIPS_TOKEN_ADDRESS = "0x456..."; // Endereço do contrato DripsToken
-    const STAKING_CONTRACT_ADDRESS = "0x789..."; // Endereço do contrato StakingContract
+    const DRINK_TOKEN_ADDRESS = "0xB8636E1026dEAB5038Fb5BfB1b5214495525c8ab"; // Endereço do contrato DrinkToken
+    const DRIPS_TOKEN_ADDRESS = "0x25e2d4663BC853E8E2a81379D35a4895ccef66d6"; // Endereço do contrato DripsToken
+    const STAKING_CONTRACT_ADDRESS = "0xE4e1A5c25D5B5a418Cdc24BCebE8F640e1109eC3"; // Endereço do contrato StakingContract
 
     drinkTokenContract = new web3.eth.Contract(DrinkTokenAbi, DRINK_TOKEN_ADDRESS);
     dripsTokenContract = new web3.eth.Contract(DripsTokenAbi, DRIPS_TOKEN_ADDRESS);
