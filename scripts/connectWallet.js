a34073525fbf51dce6a734f650d60ab9448ed7d5
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-28 14:04:49 -0300

    Update connectWallet.js

--- scripts/connectWallet.js
+++ scripts/connectWallet.js
@@ -18,18 +18,22 @@ document.addEventListener('DOMContentLoaded', () => {
     disableButtons();
 
     connectButton.addEventListener('click', async () => {
-        console.log("Botão de conexão clicado."); // Log ao clicar no botão
+        console.log("Botão de conexão clicado.");
         await connectWallet();
     });
 
     async function connectWallet() {
         if (window.ethereum) {
             try {
+                console.log("MetaMask detectado.");
                 const provider = window.ethereum;
                 web3 = new Web3(provider);
+                console.log("Web3 inicializado com o provedor MetaMask.");
                 await provider.request({ method: 'eth_requestAccounts' });
+                console.log("Contas solicitadas.");
                 const accounts = await web3.eth.getAccounts();
                 userAccount = accounts[0];
+                console.log(`Conta conectada: ${userAccount}`);
                 connectButton.innerText = `Conectado: ${userAccount.slice(0, 6)}...${userAccount.slice(-4)}`;
                 connectButton.disabled = true; // Desativa o botão após a conexão
 
