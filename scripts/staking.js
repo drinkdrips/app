0b6b3eef511c1d6c3e8d04dce6bdd732f55d30e2
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-28 11:09:47 -0300

    Create staking.js

---
+++ scripts/staking.js
@@ -0,0 +1,20 @@
+import Web3 from 'web3';
+
+const web3 = new Web3(Web3.givenProvider);
+
+const stakingContractABI = [ /* ABI do StakingContract */ ];
+const stakingContractAddress = '0x...'; // Endereço do contrato StakingContract
+
+const stakingContract = new web3.eth.Contract(stakingContractABI, stakingContractAddress);
+
+export async function stakeTokens(fromAddress, amount) {
+    return await stakingContract.methods.stake(amount).send({ from: fromAddress });
+}
+
+export async function unstakeTokens(fromAddress, amount) {
+    return await stakingContract.methods.unstake(amount).send({ from: fromAddress });
+}
+
+export async function claimDrips(fromAddress) {
+    return await stakingContract.methods.claimDrips().send({ from: fromAddress });
+}
