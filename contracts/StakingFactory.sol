454d24d18489da4590b2c3b1bf4f913cdd07e260
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-28 10:11:55 -0300

    Update StakingFactory.sol
    Utilização das bibliotecas atualizadas do OpenZeppelin:
    https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contracts/

--- contracts/StakingFactory.sol
+++ contracts/StakingFactory.sol
@@ -1,13 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "./StakingContract.sol";
 
 contract StakingFactory {
-    event StakingContractDeployed(address indexed contractAddress);
+    event StakingContractDeployed(address indexed contractAddress, address indexed owner);
 
     function deployStakingContract(address _drinkToken, address _dripsToken) external {
         StakingContract stakingContract = new StakingContract(_drinkToken, _dripsToken);
-        emit StakingContractDeployed(address(stakingContract));
+        stakingContract.transferOwnership(msg.sender); // Transfer ownership to the deployer
+        emit StakingContractDeployed(address(stakingContract), msg.sender);
     }
 }
