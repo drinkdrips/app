f6f455228a3d207dad81ad3ce0cfe554934b7c01
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-05-31 21:16:27 -0300

    Update interactions.js

--- scripts/interactions.js
+++ scripts/interactions.js
@@ -1,92 +1,102 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
     // Adiciona event listeners aos formulários
 
     document.getElementById('refreshBalancesBtn').addEventListener('click', async (event) => {
         event.preventDefault();
-        const userAccount = window.ethereum.selectedAddress;
-        if (userAccount) {
-            await refreshBalances();
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            if (userAccount) {
+                await refreshBalances();
+            }
+        } catch (error) {
+            console.error('Erro ao obter contas:', error.message);
         }
     });
     
     document.getElementById('buyDrinksForm').addEventListener('submit', async (event) => {
         event.preventDefault();
-        const amount = document.getElementById('tokenAmount').value;
-        const paymentToken = document.getElementById('paymentToken').value;
-        const userAccount = window.ethereum.selectedAddress;
-        if (userAccount) {
-            try {
+        try {
+            const amount = document.getElementById('tokenAmount').value;
+            const paymentToken = document.getElementById('paymentToken').value;
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            if (userAccount) {
                 await window.buyTokens(amount);
                 await refreshBalances();
-            } catch (error) {
-                console.error('Erro ao comprar tokens:', error.message);
+            } else {
+                console.error('Usuário não está conectado');
             }
-        } else {
-            console.error('Usuário não está conectado');
+        } catch (error) {
+            console.error('Erro ao comprar tokens:', error.message);
         }
     });
 
     document.getElementById('approveDrinksForm').addEventListener('submit', async (event) => {
         event.preventDefault();
-        const amountInTokens = document.getElementById('approveAmount').value;
-        const userAccount = window.ethereum.selectedAddress;
-        const spenderAddress = stakingContractAddress;
-        if (userAccount) {
-            try {
+        try {
+            const amountInTokens = document.getElementById('approveAmount').value;
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            const spenderAddress = stakingContractAddress;
+            if (userAccount) {
                 await window.approve(spenderAddress, amountInTokens);
                 console.log('Aprovação bem-sucedida para stake de tokens');
-            } catch (error) {
-                console.error('Erro ao aprovar stake de tokens:', error.message);
+            } else {
+                console.error('Usuário não está conectado');
             }
-        } else {
-            console.error('Usuário não está conectado');
+        } catch (error) {
+            console.error('Erro ao aprovar stake de tokens:', error.message);
         }
     });
 
     document.getElementById('stakeDrinksForm').addEventListener('submit', async (event) => {
         event.preventDefault();
-        const amount = document.getElementById('stakeAmount').value;
-        const userAccount = window.ethereum.selectedAddress;
-        if (userAccount) {
-            try {
+        try {
+            const amount = document.getElementById('stakeAmount').value;
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            if (userAccount) {
                 await window.stakeTokens(amount);
                 await refreshBalances();
-            } catch (error) {
-                console.error('Erro ao fazer staking de tokens:', error.message);
+            } else {
+                console.error('Usuário não está conectado');
             }
-        } else {
-            console.error('Usuário não está conectado');
+        } catch (error) {
+            console.error('Erro ao fazer staking de tokens:', error.message);
         }
     });
 
     document.getElementById('unstakeDrinksForm').addEventListener('submit', async (event) => {
         event.preventDefault();
-        const amount = document.getElementById('unstakeAmount').value;
-        const userAccount = window.ethereum.selectedAddress;
-        if (userAccount) {
-            try {
+        try {
+            const amount = document.getElementById('unstakeAmount').value;
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            if (userAccount) {
                 await window.unstakeTokens(amount);
                 await refreshBalances();
-            } catch (error) {
-                console.error('Erro ao realizar o unstake:', error.message);
+            } else {
+                console.error('Usuário não está conectado');
             }
-        } else {
-            console.error('Usuário não está conectado');
+        } catch (error) {
+            console.error('Erro ao realizar o unstake:', error.message);
         }
     });
 
     document.getElementById('claimDripsForm').addEventListener('submit', async (event) => {
         event.preventDefault();
-        const userAccount = window.ethereum.selectedAddress;
-        if (userAccount) {
-            try {
+        try {
+            const accounts = await window.ethereum.request({ method: 'eth_accounts' });
+            const userAccount = accounts[0]; // Assumindo que você deseja usar a primeira conta
+            if (userAccount) {
                 await window.claimRewards();
                 await refreshBalances();
-            } catch (error) {
-                console.error('Erro ao reivindicar recompensas:', error.message);
+            } else {
+                console.error('Usuário não está conectado');
             }
-        } else {
-            console.error('Usuário não está conectado');
+        } catch (error) {
+            console.error('Erro ao reivindicar recompensas:', error.message);
         }
     });
 });
