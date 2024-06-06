41118fe94c99e25cab1635ddec5f76e8f91de04c
DrinkDrips <153828385+drinkdrips@users.noreply.github.com>
2024-06-06 15:53:46 -0300

    Update DrinkToken.sol

--- contracts/DrinkToken.sol
+++ contracts/DrinkToken.sol
@@ -10,16 +10,29 @@ contract DrinkToken is ERC20, Ownable, ReentrancyGuard, Pausable {
     uint256 public constant MAX_SUPPLY = 51_000_000 * (10 ** uint256(18));
     uint256 public constant PRE_SALE_SUPPLY = 30_000_000 * (10 ** uint256(18));
     uint256 public constant DRINK_PRICE_USD = 10; // $0.10 por Drink token
+    uint256 public constant WHITELIST_DRINK_PRICE_USD = 5; // $0.05 por Drink token para whitelist
     bool public presaleActive = true;
 
     mapping(address => uint256) public tokensPurchased;
+    mapping(address => bool) public whiteList;
 
     event TokensPurchased(address indexed buyer, address indexed token, uint256 amountPaid, uint256 amountBought);
+    event AddressWhiteListed(address indexed account, bool isWhiteListed);
 
     constructor() ERC20("DrinkToken", "DRINK") {}
 
+    modifier onlyWhiteListed() {
+        require(whiteList[msg.sender], "Not white listed");
+        _;
+    }
+
     function buyTokensWithUsd(uint256 usdAmount) external whenNotPaused nonReentrant {
-        uint256 tokenAmount = (usdAmount * 100) * (10 ** uint256(18)) / DRINK_PRICE_USD;
+        uint256 tokenAmount;
+        if (whiteList[msg.sender] && presaleActive) {
+            tokenAmount = (usdAmount * 100) * (10 ** uint256(18)) / WHITELIST_DRINK_PRICE_USD;
+        } else {
+            tokenAmount = (usdAmount * 100) * (10 ** uint256(18)) / DRINK_PRICE_USD;
+        }
         _handlePurchase(tokenAmount);
         emit TokensPurchased(msg.sender, address(0), usdAmount, tokenAmount);
     }
@@ -40,9 +53,18 @@ contract DrinkToken is ERC20, Ownable, ReentrancyGuard, Pausable {
     }
 
     function mintTokens(uint256 amount) external onlyOwner {
-        require(!presaleActive, "Cannot mint during presale");
         require(totalSupply() + amount <= MAX_SUPPLY, "Exceeds maximum supply");
         _mint(msg.sender, amount);
+    }
+
+    function addToWhiteList(address account) external onlyOwner {
+        whiteList[account] = true;
+        emit AddressWhiteListed(account, true);
+    }
+
+    function removeFromWhiteList(address account) external onlyOwner {
+        whiteList[account] = false;
+        emit AddressWhiteListed(account, false);
     }
 
     function pause() external onlyOwner {
